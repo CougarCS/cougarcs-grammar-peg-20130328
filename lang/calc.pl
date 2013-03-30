ea7a251d0704c3b6b0c2861b7657b7a55b1c1875
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-03-30 15:15:47 -0500

    add assignment to Perl version

--- lang/calc.pl
+++ lang/calc.pl
@@ -16,8 +16,7 @@ stmt: expr endstmt
 expr: add_sub | assign
 assign: variable opassign add_sub
 add_sub: mul_div+ % /~([<PLUS><DASH>])~/
-mul_div: exp+ % /~([<STAR><SLASH>])~/
-exp: token+ % /~<CARET>~/
+mul_div: token+ % /~([<STAR><SLASH>])~/
 token: /~<LPAREN>~/ expr /~<RPAREN>~/ | number | variable !opassign
 
 # Lexemes
@@ -30,60 +29,76 @@ w: /[<SPACE><TAB>]*/
 END
 ;
 
+my $variables = [ (0) x 26 ]; # used in get_op_as_number and actions
 {
-    package Calculator;
-    use base 'Pegex::Tree';
+	package Calculator;
+	use base 'Pegex::Tree';
 
-    sub got_add_sub {
-        my ($self, $list) = @_;
-        $self->flatten($list);
-        while (@$list > 1) {
-            my ($a, $op, $b) = splice(@$list, 0, 3);
-            unshift @$list, ($op eq '+') ? ($a + $b) : ($a - $b);
-        }
-        @$list;
-    }
+	sub got_add_sub {
+		my ($self, $list) = @_;
+		$self->flatten($list);
+		while (@$list > 1) {
+			my ($a, $op, $b) = splice(@$list, 0, 3);
+			unshift @$list, ($op eq '+') ?
+				($self->get_op_as_number($a) + $self->get_op_as_number($b))
+				: ($self->get_op_as_number($a) - $self->get_op_as_number($b));
+		}
+		@$list;
+	}
 
-    sub got_mul_div {
-        my ($self, $list) = @_;
-        $self->flatten($list);
-        while (@$list > 1) {
-            my ($a, $op, $b) = splice(@$list, 0, 3);
-            unshift @$list, ($op eq '*') ? ($a * $b) : ($a / $b);
-        }
-        @$list;
-    }
+	sub got_mul_div {
+		my ($self, $list) = @_;
+		$self->flatten($list);
+		while (@$list > 1) {
+			my ($a, $op, $b) = splice(@$list, 0, 3);
+			unshift @$list, ($op eq '*') ?
+				($self->get_op_as_number($a) * $self->get_op_as_number($b))
+				: ($self->get_op_as_number($a) / $self->get_op_as_number($b));
+		}
+		if(@$list == 1 and $list->[0] =~ /[a-z]/) { # if the list was just a single variable
+			$list->[0] = $self->get_op_as_number($list->[0])
+		}
+		@$list;
+	}
 
-    sub got_exp {
-        my ($self, $list) = @_;
-        $self->flatten($list);
-        while (@$list > 1) {
-            my ($a, $b) = splice(@$list, -2, 2);
-            push @$list, $a ** $b;
-        }
-        @$list;
-    }
+	sub get_op_as_number {
+			my ($self, $op) = @_;
+			if( $op =~ /[a-z]/) {
+					return $variables->[ord($op) - ord('a')];
+			}
+			return $op;
+	}
+
+	sub got_assign {
+		my ($self, $list) = @_;
+		$self->flatten($list);
+		my ($a, $eq, $b) = splice(@$list, 0, 3);
+		$variables->[ord($a) - ord('a')] = $self->get_op_as_number($b);
+		push $list, $variables->[ord($a) - ord('a')];
+		@$list;
+	}
 }
 
 
 my $calculator = pegex($grammar, receiver => 'Calculator');
 my $pegex_tree = pegex($grammar, receiver => 'Pegex::Tree');
 
-my $term = Term::ReadLine->new('Calculator');
+my $term = Term::ReadLine->new('Calculator', \*STDIN, \*STDOUT);
 $term->ornaments(0);
 
 while ( defined ($_ = $term->readline('> ')) ) {
 	chomp(my $expr = $_);
-    my $result = eval { [ $calculator->parse($expr), $pegex_tree->parse($expr) ] };
+	my $result = eval { [ $calculator->parse($expr), $pegex_tree->parse($expr) ] };
 	if($@) {
 		print $@
 	} else {
 		for my $statement (0..@{$result->[0]}) {
 			my $calc = Pegex::Tree->flatten($result->[0])->[$statement];
-			my $tree_str = $result->[1]->[$statement];
+			next unless defined $calc;
+			my $tree_str = $result->[1]->[$statement]->[0];
 			my $comment_tree = p( $tree_str ) =~ s/^/#/gmr;
 			print "$calc\n";
-			print "$comment_tree\n";
+			#print "$comment_tree\n";
 		}
 	}
 }
